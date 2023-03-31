e9da4deb073e83fffa77596eeb1c7cf13621767c
Clemente Gotelli <96237432+cgotelli@users.noreply.github.com>
2023-03-31 15:33:20 +0200

    New way of computing time series
    Now it doesn't take a mean but it takes one value every minute and computes everything.

--- getBedload.m
+++ getBedload.m
@@ -1,5 +1,5 @@
 function [bedloadTable, aggTable]= getBedload(Path, plotSedRate, ...
-    plotCumSed, plotBoth, originaldt, newdt)
+    plotCumSed, plotBoth, originaldt, lag)
 % GETBEDLOAD
 % This function reads a file and returns two timetables: the raw table with
 % the default dt (10 seconds), and other with a frequency defined by the
@@ -14,9 +14,9 @@ inputTable = addvars(inputTable,values,'NewVariableNames','Weight');
 inputTable = addvars(inputTable,values-values(1),'NewVariableNames',...
     'CumBedload');
 
-% Second table with different dt
-aggTable = inputTable;
-aggTable = retime(aggTable,'regular', 'spline','SampleRate',1/newdt);
+% % Second table with different dt
+aggTable = inputTable(1:lag:end,:);
+% aggTable = retime(aggTable,'regular', 'spline','SampleRate',1/newdt);
 
 % Adding new columns for bedload rates.
 % First in the original table.
@@ -38,13 +38,13 @@ bedloadTable = addvars(bedloadTable, time(between(bedloadTable.Time(1),...
 % Now for the new table with different dt.
 bedloadVar = diff(aggTable.Weight); % We compute the instant variation [V]
 aggTable = addvars(aggTable,[0;bedloadVar],'NewVariableNames','deltaBedload');
-aggTable = addvars(aggTable,aggTable.deltaBedload./newdt,...
+aggTable = addvars(aggTable,aggTable.deltaBedload./lag,...
     'NewVariableNames','BedloadRate');
 
 bedloadVar(bedloadVar(:)<0) = 0; % We remove negative variations
 aggTable = addvars(aggTable,[0;bedloadVar],'NewVariableNames',...
     'PositiveDeltaBedload');
-aggTable = addvars(aggTable,aggTable.PositiveDeltaBedload./newdt,...
+aggTable = addvars(aggTable,aggTable.PositiveDeltaBedload./lag,...
     'NewVariableNames','PositiveBedloadRate');
 aggTable = addvars(aggTable, time(between(aggTable.Time(1),...
     aggTable.Time(:))),'NewVariableNames','dt');
@@ -53,32 +53,32 @@ aggTable = addvars(aggTable, time(between(aggTable.Time(1),...
 % Plotting
 if plotSedRate
     figure()
-    plot(bedloadTable.Time,bedloadTable.BedloadRate,...
-        DisplayName="Bedload Rate Original")
+    plot(aggTable.Time,aggTable.PositiveBedloadRate, ...
+            DisplayName=strcat("Bedload Rate Agg at ",num2str(lag), "s"))
     title(Path(end-11:end))
     xlabel("Time [hh:mm]")
     ylabel("Bedload rate [g/s]")
     if plotBoth
         disp("Plotting both")
         hold on
-        plot(aggTable.Time,aggTable.BedloadRate, ...
-            DisplayName=strcat("Bedload Rate Agg at ",num2str(newdt), "s"))
+        plot(bedloadTable.Time,bedloadTable.PositiveBedloadRate,...
+        DisplayName="Bedload Rate Original")
         legend()
     end
 end
 
 if plotCumSed
     figure()
-    plot(bedloadTable.Time,bedloadTable.CumBedload,...
-        DisplayName="Bedload Rate Original")
+    plot(aggTable.Time,aggTable.CumBedload, ...
+        DisplayName=strcat("Bedload Rate Agg at ",num2str(lag), "s"))
     title(Path(end-11:end))
     xlabel("Time [hh:mm]")
     ylabel("Accumulated Bedload")
     if plotBoth
         disp("Plotting both")
         hold on
-        plot(aggTable.Time,aggTable.CumBedload, ...
-            DisplayName=strcat("Bedload Rate Agg at ",num2str(newdt), "s"))
+        plot(bedloadTable.Time,bedloadTable.CumBedload,...
+        DisplayName="Bedload Rate Original")
         legend()
     end
 
--- main_bedload.m
+++ main_bedload.m
@@ -15,9 +15,9 @@ filesPath = "D:\git\cgotelli\ForceSensorControl\logfiles"; % Where the Logfiles 
 
 plotSedRate = true;         % Plot sediment rate timeseries for each file
 plotCumSed = true;          % Plot accumulated sediment timeseries for each file
-plotBoth = true;            % Plot the smoothed series in addition to the original
-originalTimeWindow = 10;    % What is the frequency at which the data was written in the logfile
-newTimeWindow = 120;         % The frequency at which the smoothness will be performed
+plotBoth = false;            % Plot the smoothed series in addition to the original
+originalTimeWindow = 1;     % What is the frequency at which the data was written in the logfile
+lag = 60;                   % The frequency at which the smoothness will be performed
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -31,15 +31,18 @@ for i=1:length(filenames)
     % Create ensembled timetable and plot individual plots for each serie
     [bedloadTable, aggTable] = getBedload(fullfile(filenames(i).folder, ...
         filenames(i).name(1:end-4)), plotSedRate, plotCumSed, ...
-        plotBoth, originalTimeWindow, newTimeWindow);
+        plotBoth, originalTimeWindow, lag);
+
     if i==1 % The first one is copied intact
-        allBLTable = [allBLTable;bedloadTable];
+        allBLTable = [allBLTable; bedloadTable];
         allAggTable = [allAggTable; aggTable];
     else    % For the rest we modify the time to make it a continuous time serie
-        bedloadTable.dt = allBLTable.dt+allBLTable.dt(end)+seconds(originalTimeWindow);
-        aggTable.dt = allAggTable.dt+allAggTable.dt(end)+seconds(newTimeWindow);
+        bedloadTable.dt = bedloadTable.dt+allBLTable.dt(end)+ ...
+            seconds(originalTimeWindow);
+        aggTable.dt = aggTable.dt+allAggTable.dt(end)+ ...
+            seconds(lag);
 
-        allBLTable = [allBLTable;bedloadTable];
+        allBLTable = [allBLTable; bedloadTable];
         allAggTable = [allAggTable; aggTable];
     end
 end
@@ -48,32 +51,37 @@ end
 % Plotting the resulting complete time series for the experiment
 % Bed load rate
 figure()
-plot(datetime("today")+allBLTable.dt,allBLTable.BedloadRate, ...
-    DisplayName="Bedload Rate Original")
+plot(datetime("today")+allAggTable.dt, allAggTable.PositiveBedloadRate, ...
+        DisplayName=strcat("Bedload Rate Agg at ",num2str(lag),...
+        "s"))
+
+hold on 
+plot(datetime("today")+allBLTable.dt, zeros(height(allBLTable),1),...
+        'k--', DisplayName='Zero')
 xlabel("Time [hh:mm]")
 ylabel("Bedload rate [g/s]")
 if plotBoth
     disp("Plotting both")
-    hold on
-    plot(datetime("today")+allAggTable.dt, allAggTable.BedloadRate, ...
-        DisplayName=strcat("Bedload Rate Agg at ",num2str(newTimeWindow),...
-        "s"))
-    plot(datetime("today")+allBLTable.dt, zeros(height(allBLTable),1),...
-        'k--', DisplayName='Zero')
+    plot(datetime("today")+allBLTable.dt,allBLTable.PositiveBedloadRate, ... 
+        DisplayName="Bedload Rate Original")
+    
     legend()
 end
 
-% Accumulated bed load
+%% Accumulated bed load
 figure()
 plot(datetime("today")+allBLTable.dt, allBLTable.CumBedload, ...
-    DisplayName="Bedload Rate Original")
+    DisplayName="All values")
+hold on
+plot(datetime("today")+allBLTable.dt(1:60:end), allBLTable.CumBedload(1:60:end), ...
+    DisplayName="1 each 60s")
+
+% plot(datetime("today")+allBLTable.dt(1:30:end), allBLTable.CumBedload(1:30:end), ...
+%     DisplayName="1 each 30s")
+% plot(datetime("today")+allBLTable.dt(1:20:end), allBLTable.CumBedload(1:20:end), ...
+%     DisplayName="1 each 20s")
+% plot(datetime("today")+allBLTable.dt(1:10:end), allBLTable.CumBedload(1:10:end), ...
+%     DisplayName="1 each 10s")
 xlabel("Time [hh:mm]")
 ylabel("Accumulated Bedload")
-if plotBoth
-    disp("Plotting both")
-    hold on
-    plot(datetime("today")+allAggTable.dt, allAggTable.CumBedload, ...
-        DisplayName=strcat("Bedload Rate Agg at ",num2str(newTimeWindow), "s"))
-    legend()
-end
-
+legend()
